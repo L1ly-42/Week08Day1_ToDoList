ac4fd41c992b11e40db7003acc9ed88064b64c2c
Jean <jean-ata@hotmail.co.uk>
2024-03-18 17:47:47 +0000

    added conmpletedButton Function that moved to-do list items to completed list

--- code/lab_jsevents_start_code/script.js
+++ code/lab_jsevents_start_code/script.js
@@ -1,33 +1,42 @@
 // TASK 1 - Taking form input and adding a corresponding list item 
 const form = document.querySelector("#todo-form");
 const list = document.querySelector("#list");
-
-// // Adding event listener onto the delete button
-// const removeItem = (item) => {
-//     item.addEventListener('click', () => {item.remove();
-//     });
-// }
+const complete = document.querySelector("#completed");
+const completedList = document.querySelector("#completedList");
 
 form.addEventListener('submit', (evt) => {
     evt.preventDefault();
     const newListItem = document.createElement('li');
     const deleteButton = document.createElement("button");
     deleteButton.textContent = 'Delete';
-    // removeItem(newListItem);
+
     newListItem.innerText = evt.target["new-todo"].value;
     newListItem.appendChild(deleteButton);
     deleteButton.addEventListener("click", () => {
         newListItem.remove();
+    });
 
-    })
+    const completeButton = document.createElement('button');
+    completeButton.textContent = 'Complete!';
+    console.log(completeButton);
+    newListItem.appendChild(completeButton);
+    completeButton.addEventListener("click", () => {
+        completedList.appendChild(newListItem);
+    });
+
+
     list.appendChild(newListItem);
 });
+// This is adding a Date Button that displays the Current Date
+const dateButton = document.querySelector("#show-date");
+const dateDisplay = document.querySelector("#date-display");
 
-const dateButton = document.querySelector("#show-date")
-const dateDisplay = document.querySelector("#dat-display")
-
-dateButton.addEventListener("click", (evt) =>{
+dateButton.addEventListener("click", (evt) => {
     const date = newDate();
     dateDisplay.innerText = new Intl.DateTimeFormat('en-UK').format(date);
 });
+
+
+
+
 
