e58bf65bee77129df03be55360e5df65900eae7c
Leila Peltier <l_peltier@outlook.com>
2024-03-18 15:34:14 +0000

    Made it so every <li> elements needs an accompanying delete button

--- code/lab_jsevents_start_code/script.js
+++ code/lab_jsevents_start_code/script.js
@@ -1,12 +1,22 @@
-// TASK 1 - attaching input into the 
-
+// TASK 1 - Taking form input and adding a corresponding list item 
 const form = document.querySelector("#todo-form");
 const list = document.querySelector("#list");
+
+// // Adding event listener onto the delete button
+// const removeItem = (item) => {
+//     item.addEventListener('click', () => {item.remove();
+//     });
+// }
 
 form.addEventListener('submit', (evt)=>{
     evt.preventDefault();
     const newListItem = document.createElement('li');
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = 'Delete';
+    // removeItem(newListItem);
     newListItem.innerText = evt.target["new-todo"].value;
+    newListItem.appendChild(deleteButton);
     list.appendChild(newListItem);
+    
 });
 
