d8bc1f88919909c350d6513899a6eca9bf4ddfcd
Leila Peltier <l_peltier@outlook.com>
2024-03-19 09:54:49 +0000

    added comments to code for readability

--- code/lab_jsevents_start_code/script.js
+++ code/lab_jsevents_start_code/script.js
@@ -6,28 +6,37 @@ const completedList = document.querySelector("#completedList");
 
 form.addEventListener('submit', (evt) => {
     evt.preventDefault();
+
+    //creating an li with corresponding input value
     const newListItem = document.createElement('li');
+    newListItem.innerText = evt.target["new-todo"].value;
+
+    //creating a delete button and appending it to li
     const deleteButton = document.createElement("button");
     deleteButton.textContent = 'Delete';
-
-    newListItem.innerText = evt.target["new-todo"].value;
     newListItem.appendChild(deleteButton);
+
+    //adding event listener to delete button that adds functionality
     deleteButton.addEventListener("click", () => {
         newListItem.remove();
     });
-
+    
+    //creating a complete button and adding it to li
     const completeButton = document.createElement('button');
     completeButton.textContent = 'Complete!';
-    console.log(completeButton);
     newListItem.appendChild(completeButton);
+    
+    // adding event listener to complete button that makes it move li to completed list
     completeButton.addEventListener("click", () => {
         completedList.appendChild(newListItem);
     });
 
-
+    // adding finished li to ul
     list.appendChild(newListItem);
 });
-// This is adding a Date Button that displays the Current Date
+
+
+// Adding a Date Button that displays the Current Date
 const dateButton = document.querySelector("#show-date");
 const dateDisplay = document.querySelector("#date-display");
 
